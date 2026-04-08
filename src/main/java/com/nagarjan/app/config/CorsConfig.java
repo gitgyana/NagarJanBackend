889e1fca5537091edb6d479ba09163034d6bd4ec
Gyana Priyadarshi <gyana.career@gmail.com>
2026-04-08 10:33:02 +0530

    Add CorsConfig and ForwardController

---
+++ src/main/java/com/nagarjan/app/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.nagarjan.app.config;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${spring.allowed.origin}")
+    private String allowedOrigin;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
---
+++ src/main/java/com/nagarjan/app/controller/ForwardController.java
@@ -0,0 +1,17 @@
+package com.nagarjan.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForwardController {
+
+    @RequestMapping(value = {
+            "/",
+            "/{path:[^\\.]*}",
+            "/**/{path:[^\\.]*}"
+    })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
